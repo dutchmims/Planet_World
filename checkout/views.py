a26c9280c7db6230c43c9725982b57f43bc48360
dutchmims <jameshealy901@gmail.com>
2024-03-30 12:46:47 +0000

    Add order history and updated checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -7,7 +7,12 @@ from .forms import OrderForm
 from .models import Order, OrderLineItem
 
 from products.models import Product
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 from bag.contexts import bag_contents
+
+import stripe
+import json
 
 
 @require_POST
