b35cc659a68395198beab7b88efdfa3695336bc4
dutchmims <jameshealy901@gmail.com>
2024-03-30 10:55:58 +0000

    Update checkout views, create webhook and handler

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
-    path('checkout/', views.checkout_view, name='checkout'),
-    path('order_confirmation/', views.order_confirmation_view, name='order_confirmation'),
+    path('', views.checkout, name='checkout'),
+    path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
+    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
+    path('wh/', webhook, name='webhook'),
 ]
