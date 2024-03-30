4df7273dc0462e22d2ceb503da794b21dd71c146
dutchmims <jameshealy901@gmail.com>
2024-03-30 12:54:05 +0000

    Update bag

--- bag/models.py
+++ bag/models.py
@@ -1,9 +1,1 @@
-# bag/models.py
 from django.db import models
-
-class Item(models.Model):
-    name = models.CharField(max_length=100)
-    quantity = models.PositiveIntegerField(default=1)
-
-    def __str__(self):
-        return self.name
