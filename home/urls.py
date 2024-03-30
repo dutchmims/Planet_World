a0e213fe2c745e0642c6259497ea90449db7ff16
dutchmims <jameshealy901@gmail.com>
2024-03-30 12:58:58 +0000

    Update home

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,6 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    # Add URL pattern for the profile page
-    path('my-profile/', views.my_profile, name='my_profile'),
-    path('profile/', views.my_profile, name='profile'),
+    path('', views.index, name='home')
 ]
--- home/views.py
+++ home/views.py
@@ -1,19 +1,8 @@
 from django.shortcuts import render
 
+# Create your views here.
+
 def index(request):
     """ A view to return the index page """
+
     return render(request, 'home/index.html')
-
-def product_management(request):
-    """ A view to display the product management page """
-    products = Product.objects.all()  
-    context = {'products': products}
-    return render(request, 'home/product_management.html', context)
-
-def my_profile(request):
-    """ A view to display the user's profile """
-    return render(request, 'home/my_profile.html')
-
-def profile_view(request):
-    """ A view to display the user's profile """
-    return render(request, 'home/my_profile.html')
