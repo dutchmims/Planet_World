2ed026ef18a44ca2696ddcc167f76f9d22f6e46b
dutchmims <jameshealy901@gmail.com>
2024-03-30 10:53:30 +0000

    Add signals.py

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,18 +1,31 @@
 from django.contrib import admin
-from .models import Item, Customer, Order
+from .models import Order, OrderLineItem
 
-# Register your models here.
 
-@admin.register(Item)
-class ItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price')
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
 
-@admin.register(Customer)
-class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('user', 'address')
 
-@admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'customer', 'total_price', 'created_at')
-    readonly_fields = ('total_price',)
-    filter_horizontal = ('items',)  # Allows selecting items in a more user-friendly way
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total', 'original_bag',
+                       'stripe_pid')
+
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total', 'original_bag',
+              'stripe_pid')
+
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
